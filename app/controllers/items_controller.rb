3fa10803eb40019941e66e5c7ef750b9ce1509dc
koichiro-maeyama <koh16.maeyama@gmail.com>
2015-06-13 16:24:44 +0900

    商品一覧と詳細ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+  def show
+    #Itemというmodelに対してfindメソッドでidを検索させている
+    @item = Item.find(params[:id])
+  end
+  def index
+    @items = Item.all
+  end
 end
